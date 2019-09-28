1698f6a23fa3fc50903f248bfdc4e79f2779ae22
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 18:27:07 -0700

    add genre_name method to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :genre
+
+  def genre_name
+    self.genre.name
+  end
 end
--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :image, :genre_id
+  attributes :id, :title, :author, :image, :genre_id, :genre_name
   belongs_to :genre
 end
