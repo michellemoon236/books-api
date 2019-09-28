400d3a13d90e319d0cc370bb4b3525916ecdc385
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 18:30:39 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create([
+  { name: 'General Fiction' },
+  { name: 'Historical Fiction' },
+  { name: 'Action/Adventure' },
+  { name: 'Classics' },
+  { name: 'Fantasy' },
+  { name: 'Mystery' },
+  { name: 'Biography' },
+  { name: 'Self Help' },
+  { name: 'Other' }
+])
+
+Book.create([
+  { title: 'Art of Racing in the Rain', author: 'Garth Stein', image: 'https://prodimage.images-bn.com/pimages/9780061537967_p0_v8_s550x406.jpg', genre_id: 1 },
+
+  { title: 'The Help', author: 'Kathryn Stockett', image: 'https://prodimage.images-bn.com/pimages/9780425232200_p0_v4_s550x406.jpg', genre_id: 2 },
+
+  { title: 'The Gifts of Imperfection', author: 'Brene Brown', image: 'https://prodimage.images-bn.com/pimages/9781592858491_p0_v6_s550x406.jpg', genre_id: 8 }
+])
