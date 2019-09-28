77f2bbe64782420958e63f782e774cda8c4c7eff
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 18:24:55 -0700

    add belongs to and has many relationships

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id
+  belongs_to :genre
 end
--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,3 +1,4 @@
 class GenreSerializer < ActiveModel::Serializer
   attributes :id
+  has_many :books
 end
